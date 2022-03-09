6e01fe6f779171c7c1b1173a6b1a5ac0220b51a2
PRASHANT KUMAR JHA <39798526+PrashantJha3@users.noreply.github.com>
2022-03-09 17:09:42 +0530

    Initial Commit with react-todo-list

---
+++ README.md
@@ -0,0 +1,18 @@
+# Prateek-todo-react-app-globant
+
+Sample todo app built with the React/ReactDOM framework. For the Company interview purpose.
+
+This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
+
+## Available Scripts
+
+First clone this repository.
+
+$ git clone url
+
+Install dependencies. Make sure you already have nodejs & npm installed in your system.
+
+$ npm install # or yarn
+Run it
+
+$ npm start # or yarn start
---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+
+const DATA = [
+  { id: "todo-0", name: "Eat", completed: true },
+  { id: "todo-1", name: "Sleep", completed: false },
+  { id: "todo-2", name: "Workout", completed: false },
+  { id: "todo-3", name: "Interview", completed: false },
+  { id: "todo-4", name: "Repeat", completed: false }
+];
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App tasks={DATA} />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
